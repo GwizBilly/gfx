61ecda8e798cef75c7edae538cd18a9fb2ce576b
Guillaume Savoie <f.flat.minor@gmail.com>
2020-09-16 23:40:37 -0400

    add two new graphics

--- g.c
+++ g.c
@@ -3,12 +3,15 @@
 	CSE 20211
 	9/7/2011
 	by Prof. Thain
+	Modified and expanded by Mr. Savoie 16/Sept/2020
 */
 #include <stdio.h>
 #include "gfx.h"
 void mySetup();
 void makeStuff(int d, int b, int r);
 void gridder(int d, int b, int r);
+void middleBox(int d, int b, int r);
+void design(int d, int b, int r);
 char sizeDecide(char n);
 #define Gsize 2 
 #define ysize 240 * Gsize
@@ -18,29 +21,65 @@ char sizeDecide(char n);
 #define buff 4 * Gsize
 #define res 77 * Gsize
 
-int	rr, dd, bb; // resolution, dimensions, border
+int	d, b, r; // resolution, dimensions, border
 
 int main()
 {
+
 	mySetup();
 	char c;
-  while (1) { // Wait for the user to press a character.
+  while (1) { // Wait for the user to press a character, to set size.
 		c = sizeDecide(c = gfx_wait());
 		if (c != 'q' && c != '0') {
-			rr = rr * Gsize;
-			bb = bb * Gsize;
-			makeStuff(dd, bb, rr);
+			r = r * Gsize;
+			b = b * Gsize;
+			makeStuff(d, b, r);
 		}		
 		if (c == 'q') break; // Quit if it is the letter q.
 	}
 	return 0;
+
 }
 
 void makeStuff(int d, int b, int r) {
 
 	gfx_clear();
-	gridder(d, b, r);
+	//gridder(d, b, r);
+	//middleBox(d, b, r);
+	design(d, b, r);
 	gfx_flush();
+
+}
+
+void design(int d, int b, int r) {
+
+	//gfx_color(0, 0, 0);
+	for (int i = 0; i < d; i++) {
+		for (int j = 0; j < d; j++) {
+			for (int k = 1; k <= 3; k++) {
+				for (int l = 1; l <= 3; l++) {
+					gfx_line((i * r + b) * k, (j * r + b) * l, i * k, j * l);
+					//gfx_point(i * r + b + k * (k++)/16, j * r + b + l);
+				}
+			}
+		}
+	} 
+	gfx_color(0, 200, 100);
+
+}
+
+void middleBox(int d, int b, int r) {
+
+	for (int i = 0; i < d; i++) {
+		for (int j = 0; j < d; j++) {
+			for (int k = 1; k < r-1; k++) {
+				for (int l = 1; l < r-1; l++) {
+					gfx_point(i * r + b + k, j * r + b + l);
+					//gfx_point(i * r + b + k * (k++)/16, j * r + b + l);
+				}
+			}
+		}
+	} 
 
 }
 
@@ -72,36 +111,38 @@ void gridder(int d, int b, int r) {
 char sizeDecide(char c) {
 
 	if (c == '3') {
-		rr = 77;
-		bb = 4;
-		dd = 3;
+		d = 3;
+		b = 4;
+		r = 77;
 	} else if (c == '5') {
-		rr = 47;
-		bb = 2;
-		dd = 5;
+		d = 5;
+		b = 2;
+		r = 47;
 	} else if (c == '7') {
-		rr = 33;
-		bb = 4;
-		dd = 7;
+		d = 7;
+		b = 4;
+		r = 33;
 	} else if (c == '9') {
-		rr = 25;
-		bb = 7;
-		dd = 9;
+		d = 9;
+		b = 7;
+		r = 25;
 	} else if (c == 'b') {
-		rr = 21;
-		bb = 4;
-		dd = 11;
+		d = 11;
+		b = 4;
+		r = 21;
 	} else if (c == 'q') {
 		return 'q';
 	} else {
 		return '0';
 	}
+
 }
 
 void mySetup() {
 
 	gfx_open(xsize, ysize, "Example Graphics Program");
 	gfx_color(0, 200, 100);
+	//make stuff with default parameters from #define variables
 	makeStuff(dim, buff, res);
 
 }
