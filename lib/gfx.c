47605c589cabc7fcf6c17db0d98e7fc3614845a8
Guillaume Savoie <g13_______g13@hotmail.com>
2021-10-21 12:31:09 -0400

    Tabs to spaces.

--- lib/gfx.c
+++ lib/gfx.c
@@ -29,101 +29,101 @@ static int saved_ypos = 0;
 // Open a new graphics window.
 
 void gfx_open(int width, int height, const char *title) {
-	gfx_display = XOpenDisplay(0);
-	if(!gfx_display) {
-		fprintf(stderr, "gfx_open: unable to open the graphics window.\n");
-		exit(1);
-	}
+  gfx_display = XOpenDisplay(0);
+  if(!gfx_display) {
+    fprintf(stderr, "gfx_open: unable to open the graphics window.\n");
+    exit(1);
+  }
 
-	Visual *visual = DefaultVisual(gfx_display, 0);
-	if(visual && visual->class == TrueColor) {
-		gfx_fast_color_mode = 1;
-	} else {
-		gfx_fast_color_mode = 0;
-	}
+  Visual *visual = DefaultVisual(gfx_display, 0);
+  if(visual && visual->class == TrueColor) {
+    gfx_fast_color_mode = 1;
+  } else {
+    gfx_fast_color_mode = 0;
+  }
 
-	int blackColor = BlackPixel(gfx_display, DefaultScreen(gfx_display));
-	int whiteColor = WhitePixel(gfx_display, DefaultScreen(gfx_display));
+  int blackColor = BlackPixel(gfx_display, DefaultScreen(gfx_display));
+  int whiteColor = WhitePixel(gfx_display, DefaultScreen(gfx_display));
 
-	gfx_window = XCreateSimpleWindow(gfx_display, DefaultRootWindow(gfx_display), 0, 0, width, height, 0, blackColor, blackColor);
+  gfx_window = XCreateSimpleWindow(gfx_display, DefaultRootWindow(gfx_display), 0, 0, width, height, 0, blackColor, blackColor);
 
-	XSetWindowAttributes attr;
-	attr.backing_store = Always;
+  XSetWindowAttributes attr;
+  attr.backing_store = Always;
 
-	XChangeWindowAttributes(gfx_display, gfx_window, CWBackingStore, &attr);
+  XChangeWindowAttributes(gfx_display, gfx_window, CWBackingStore, &attr);
 
-	XStoreName(gfx_display, gfx_window, title);
+  XStoreName(gfx_display, gfx_window, title);
 
-	XSelectInput(gfx_display, gfx_window, StructureNotifyMask|KeyPressMask|ButtonPressMask|PointerMotionMask);
+  XSelectInput(gfx_display, gfx_window, StructureNotifyMask|KeyPressMask|ButtonPressMask|PointerMotionMask);
 
-	XMapWindow(gfx_display, gfx_window);
+  XMapWindow(gfx_display, gfx_window);
 
-	gfx_gc = XCreateGC(gfx_display, gfx_window, 0, 0);
+  gfx_gc = XCreateGC(gfx_display, gfx_window, 0, 0);
 
-	gfx_colormap = DefaultColormap(gfx_display, 0);
+  gfx_colormap = DefaultColormap(gfx_display, 0);
 
-	XSetForeground(gfx_display, gfx_gc, whiteColor);
+  XSetForeground(gfx_display, gfx_gc, whiteColor);
 
-	// Wait for the MapNotify event.
+  // Wait for the MapNotify event.
 
-	for(;;) {
-		XEvent e;
-		XNextEvent(gfx_display, &e);
-		if (e.type == MapNotify) {break;}
-	}
+  for(;;) {
+    XEvent e;
+    XNextEvent(gfx_display, &e);
+    if (e.type == MapNotify) {break;}
+  }
 }
 
 // Draw a single point at (x,y).
 
 void gfx_point( int x, int y ) {
-	XDrawPoint(gfx_display, gfx_window, gfx_gc, x, y);
+  XDrawPoint(gfx_display, gfx_window, gfx_gc, x, y);
 }
 
 // Draw a line from (x1,y1) to (x2,y2). 
 
 void gfx_line( int x1, int y1, int x2, int y2 ) {
-	XDrawLine(gfx_display,gfx_window,gfx_gc,x1,y1,x2,y2);
+  XDrawLine(gfx_display,gfx_window,gfx_gc,x1,y1,x2,y2);
 }
 
 // Change the current drawing color.
 
 void gfx_color( int r, int g, int b ) {
-	XColor color;
+  XColor color;
 
-	if(gfx_fast_color_mode) {
-		// If this is a truecolor display, we can just pick the color directly.
-		color.pixel = ((b&0xff) | ((g&0xff)<<8) | ((r&0xff)<<16));
-	} else {
-		// Otherwise, we have to allocate it from the colormap of the display.
-		color.pixel = 0;
-		color.red   = r<<8;
-		color.green = g<<8;
-		color.blue  = b<<8;
-		XAllocColor(gfx_display, gfx_colormap, &color);
-	}
+  if(gfx_fast_color_mode) {
+    // If this is a truecolor display, we can just pick the color directly.
+    color.pixel = ((b&0xff) | ((g&0xff)<<8) | ((r&0xff)<<16));
+  } else {
+    // Otherwise, we have to allocate it from the colormap of the display.
+    color.pixel = 0;
+    color.red   = r<<8;
+    color.green = g<<8;
+    color.blue  = b<<8;
+    XAllocColor(gfx_display, gfx_colormap, &color);
+  }
 
-	XSetForeground(gfx_display, gfx_gc, color.pixel);
+  XSetForeground(gfx_display, gfx_gc, color.pixel);
 }
 
 // Clear the graphics window to the background color.
 
 void gfx_clear() {
-	XClearWindow(gfx_display, gfx_window);
+  XClearWindow(gfx_display, gfx_window);
 }
 
 // Change the current background color.
 
 void gfx_clear_color(int r, int g, int b) {
-	XColor color;
-	color.pixel = 0;
-	color.red   = r<<8;
-	color.green = g<<8;
-	color.blue  = b<<8;
-	XAllocColor(gfx_display, gfx_colormap, &color);
+  XColor color;
+  color.pixel = 0;
+  color.red   = r<<8;
+  color.green = g<<8;
+  color.blue  = b<<8;
+  XAllocColor(gfx_display, gfx_colormap, &color);
 
-	XSetWindowAttributes attr;
-	attr.background_pixel = color.pixel;
-	XChangeWindowAttributes(gfx_display, gfx_window, CWBackPixel, &attr);
+  XSetWindowAttributes attr;
+  attr.background_pixel = color.pixel;
+  XChangeWindowAttributes(gfx_display, gfx_window, CWBackPixel, &attr);
 }
 
 int gfx_event_waiting() {
@@ -154,35 +154,35 @@ int gfx_event_waiting() {
 // Wait for the user to press a key or mouse button.
 
 char gfx_wait() {
-	XEvent event;
+  XEvent event;
 
-	while (1) {
-		XNextEvent(gfx_display, &event);
+  while (1) {
+    XNextEvent(gfx_display, &event);
 
-		if (event.type == KeyPress) {
-			return XLookupKeysym(&event.xkey, 0);
-		} else if (event.type == ButtonPress) {
-			return event.xbutton.button;
-		} else if (event.type == MotionNotify) {
+    if (event.type == KeyPress) {
+      return XLookupKeysym(&event.xkey, 0);
+    } else if (event.type == ButtonPress) {
+      return event.xbutton.button;
+    } else if (event.type == MotionNotify) {
       // insert mouse pointer detection GwizBilly 2020.
       return '0';
     }
-	}
+  }
 }
 
 // Return the X and Y coordinates of the last event.
 
 int gfx_xpos() {
-	return saved_xpos;
+  return saved_xpos;
 }
 
 int gfx_ypos() {
-	return saved_ypos;
+  return saved_ypos;
 }
 
 // Flush all previous output to the window.
 
 void gfx_flush() {
-	XFlush(gfx_display);
+  XFlush(gfx_display);
 }
 
